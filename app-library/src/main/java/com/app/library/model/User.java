dffd3bcd1b8c8c8c6d8ba1fcec64aa489c55a071
DemoBoss <lukasz3287@o2.pl>
2019-05-03 16:56:14 +0100

    tworzenie rejestracji2 - nazwa surname poprawiona

--- app-library/src/main/java/com/app/library/model/User.java
+++ app-library/src/main/java/com/app/library/model/User.java
@@ -57,29 +57,7 @@ public class User {
         this.mail = mail;
     }
 
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
+    
 
     public String getPassword() {
         return password;
