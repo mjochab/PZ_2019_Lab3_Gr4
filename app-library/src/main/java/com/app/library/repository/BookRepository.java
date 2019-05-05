c0b9ef7023e957e82e8661ab659b86fa84189945
Adam <adamkrupa96@gmail.com>
2019-05-05 12:33:31 +0200

    add feature to find books by employee

--- app-library/src/main/java/com/app/library/repository/BookRepository.java
+++ app-library/src/main/java/com/app/library/repository/BookRepository.java
@@ -3,12 +3,17 @@ package com.app.library.repository;
 
 import com.app.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
-      List<Book> findAll();
-
-
+      @Query("SELECT book " +
+              "FROM Book book " +
+              "WHERE    book.name LIKE %:query% " +
+              "OR       book.author LIKE %:query% " +
+              "OR       book.publishingCompany LIKE %:query%")
+      List<Book> findByQuery(@Param("query") String query);
 }
--- app-library/src/main/java/com/app/library/service/BookService.java
+++ app-library/src/main/java/com/app/library/service/BookService.java
@@ -15,17 +15,13 @@ public class BookService {
 
     private BookRepository bookRepository;
 
-    public Book findById(Integer id) {
-        return bookRepository.getOne(id);
-    }
+    public Book findById(Integer id) { return bookRepository.getOne(id); }
 
-    public List<Book> findAll() {
-        return bookRepository.findAll();
-    }
+    public List<Book> findAll() { return bookRepository.findAll(); }
 
-    public Book saveBook(@Valid Book book) {
-        return bookRepository.save(book);
-    }
+    public List<Book> findByQuery(String query) { return bookRepository.findByQuery(query); }
+
+    public Book saveBook(@Valid Book book) { return bookRepository.save(book); }
 
     @Autowired
     public void setBookRepository(BookRepository bookRepository) {
