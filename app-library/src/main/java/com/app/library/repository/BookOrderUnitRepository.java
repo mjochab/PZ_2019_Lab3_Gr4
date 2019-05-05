a282779c63118cfd299234055ed662997ae8d5cf
Adam <adamkrupa96@gmail.com>
2019-05-06 00:24:14 +0200

    I19L3GR4-61 #comment add repositories and service for books order. add feature to find orders by query and between the dates of created at

---
+++ app-library/src/main/java/com/app/library/repository/BookOrderUnitRepository.java
@@ -0,0 +1,9 @@
+package com.app.library.repository;
+
+import com.app.library.model.BookOrderUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookOrderUnitRepository extends JpaRepository<BookOrderUnit, Integer> {
+}
---
+++ app-library/src/main/java/com/app/library/repository/BooksOrderRepository.java
@@ -0,0 +1,25 @@
+package com.app.library.repository;
+
+import com.app.library.model.BooksOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface BooksOrderRepository extends JpaRepository<BooksOrder, Integer> {
+
+    @Query("SELECT o FROM BooksOrder o " +
+            "INNER JOIN User user ON user.id = o.reader.id " +
+            "WHERE  (lower(user.firstName) LIKE %:query% OR lower(user.surname) LIKE %:query%) " +
+            "AND (" +
+            "       (o.createdAt >= :orderDateFrom OR :orderDateFrom = null) " +
+            "   AND (o.createdAt <= :orderDateTo OR :orderDateTo = null)" +
+            ")")
+    List<BooksOrder> findByQueryAndCreatedAtBetween(@Param("query") String query,
+                                                    @Param("orderDateFrom") Date orderDateFrom,
+                                                    @Param("orderDateTo") Date orderDateTo);
+}
