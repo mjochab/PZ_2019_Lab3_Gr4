7c203858ee4642a4e733204550b2e8c192e3fb75
Adam <adamkrupa96@gmail.com>
2019-05-12 23:08:37 +0200

    I19L3GR4-73 #comment add test class for string utils

---
+++ library-signature-generator/src/test/java/utils/StringUtilsTest.java
@@ -0,0 +1,18 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    @Test
+    void isNotBlank() {
+        assertTrue(StringUtils.isNotBlank("text"));
+    }
+
+    @Test
+    void isBlank() {
+        assertFalse(StringUtils.isNotBlank(null));
+    }
+}
