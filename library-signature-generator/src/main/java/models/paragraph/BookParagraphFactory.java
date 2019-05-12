575ac2ec1b1c04b7ce33b367eded418d907d6196
Adam <adamkrupa96@gmail.com>
2019-05-12 23:31:17 +0200

    I19L3GR4-73 #comment add fix for polish characters in generated document

--- library-signature-generator/src/main/java/models/paragraph/BookParagraphFactory.java
+++ library-signature-generator/src/main/java/models/paragraph/BookParagraphFactory.java
@@ -4,22 +4,23 @@ import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.BaseFont;
 
 import java.util.Objects;
 
 public class BookParagraphFactory {
 
-    private static final Font normalFont = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
-    private static final Font titleFont = new Font(Font.FontFamily.COURIER, 16, Font.BOLD, BaseColor.BLACK);
+    private static final Font HELVETICA_14 = FontFactory.getFont(FontFactory.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED, 14, Font.UNDEFINED, BaseColor.BLACK);
+    private static final Font HELVETICA_16_BOLD = FontFactory.getFont(FontFactory.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED, 16, Font.BOLD, BaseColor.BLACK);
 
     public static BookParagraph createParagraph(ParagraphType type, String content) {
 
         switch (type) {
-            case TITLE: return new TitleParagraph(Element.ALIGN_LEFT, titleFont, Objects.requireNonNull(content));
-            case AUTHOR: return new AuthorParagraph(Element.ALIGN_LEFT, normalFont, content);
-            case PUBISHING_COMPANY: return new PublishingCompanyParagraph(Element.ALIGN_LEFT, normalFont, content);
-            case YEAR_OF_PUBLICATION: return new YearOfPublicationParagraph(Element.ALIGN_LEFT, normalFont, Objects.requireNonNull(content));
-            case SIGNATURE: return new SignatureParagraph(Element.ALIGN_JUSTIFIED, normalFont, Objects.requireNonNull(content));
+            case TITLE: return new TitleParagraph(Element.ALIGN_LEFT, HELVETICA_16_BOLD, Objects.requireNonNull(content));
+            case AUTHOR: return new AuthorParagraph(Element.ALIGN_LEFT, HELVETICA_14, content);
+            case PUBISHING_COMPANY: return new PublishingCompanyParagraph(Element.ALIGN_LEFT, HELVETICA_14, content);
+            case YEAR_OF_PUBLICATION: return new YearOfPublicationParagraph(Element.ALIGN_LEFT, HELVETICA_14, Objects.requireNonNull(content));
+            case SIGNATURE: return new SignatureParagraph(Element.ALIGN_JUSTIFIED, HELVETICA_14, Objects.requireNonNull(content));
 
             default: throw new IllegalArgumentException("Paragraph type is unknown");
         }
