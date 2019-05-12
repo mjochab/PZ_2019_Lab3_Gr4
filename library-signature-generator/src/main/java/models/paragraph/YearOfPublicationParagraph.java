4675ca9b4f53fe820ff79e5413b5fdf1b9f6b937
Adam <adamkrupa96@gmail.com>
2019-05-12 23:07:49 +0200

    I19L3GR4-73 #comment add simple factory of book paragraphs

---
+++ library-signature-generator/src/main/java/models/paragraph/YearOfPublicationParagraph.java
@@ -0,0 +1,24 @@
+package models.paragraph;
+
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+
+class YearOfPublicationParagraph extends BookParagraph {
+
+    YearOfPublicationParagraph(Integer alignment, Font font, String content) {
+        super(alignment, font, content);
+        try {
+            Integer.parseInt(content);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("Year of publication must be number");
+        }
+    }
+
+    @Override
+    public Paragraph getParagraph() {
+        Paragraph p = new Paragraph(String.format("Rok publikacji: %s", content), font);
+        p.setAlignment(alignment);
+
+        return p;
+    }
+}
